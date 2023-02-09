5047386c2b20cf48af5d0b81ea86cfe46fe7daa9
RuhDel <rideleon@student.uiwtx.edu>
2023-02-09 00:53:44 -0600

    pp

--- scapy/netcat.py
+++ scapy/netcat.py
@@ -17,8 +17,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description = 'BHP Net Tool',
         formatter_class = argparse.RawDescriptionHelpFormatter,
-        epilog = textwrap.dedent('''Example: netcat.py -t 192.16.1.108 -p 5555 -l -c #command shell''')
+        epilog = textwrap.dedent('''Example: netcat.py -t 192.16.1.108 -p 5555 -l -c #command shell
+                                 netcat.py -t 192.16.1.108 -p 5555 -l -u=mytest.test #upload file''')
     )
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l')
+    parser.add_argument('-l', '--listen', action= 'store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', dafault='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
